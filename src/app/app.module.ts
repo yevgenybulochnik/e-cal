d0651d29c5b7556d94511593462cd1b5297fa97a
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-11-26 15:05:11 -0800

    ContentComponent initial setup + MatTabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatTabsModule,
+} from '@angular/material';
 
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopMenuComponent,
-    LeftMenuComponent
+    LeftMenuComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/content/content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cal-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.scss']
+})
+export class ContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
