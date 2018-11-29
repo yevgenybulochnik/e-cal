187c9f99e8b20aa8233c92d0ab9a8fc192766f95
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2018-11-28 21:16:30 -0800

    FileLoadDialog, handle input file and display sample data

--- src/app/fileload-dialog/fileload-dialog.component.ts
+++ src/app/fileload-dialog/fileload-dialog.component.ts
@@ -1,22 +1,16 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { csvParse } from 'd3';
 
 @Component({
   selector: 'cal-fileload-dialog',
-  template: `
-    <p>
-      fileload-dialog works!
-    </p>
-    <button mat-stroked-button (click)='closeDialog()'>Cancel</button>
-    <button mat-stroked-button (click)="fileInput.click()">
-      <span>Select</span>
-      <input #fileInput type="file" (change)="onFileInput($event)" style="display:none;" />
-    </button>
-  `,
-  styles: []
+  templateUrl: './fileload-dialog.component.html',
+  styleUrls: ['./fileload-dialog.component.scss']
 })
 export class FileLoadDialogComponent implements OnInit {
-
+  filePlaceHolder = 'No File Selected';
+  fileColumns: string[];
+  fileDataRows: any[];
   constructor(
     public dialogRef: MatDialogRef<FileLoadDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
@@ -25,6 +19,32 @@ export class FileLoadDialogComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  // Function needs to be refactored, split out reader.onload callback
+  onFileInput(files: FileList): void {
+    const file = files[0];
+    if (file) {
+      this.filePlaceHolder = file.name;
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const fileByteString = reader.result;
+        const parsedData = csvParse(fileByteString, (rowData, index) => {
+          if ( index < 5  ) { return rowData; }
+        });
+        this.fileColumns = parsedData.columns;
+        this.fileDataRows = parsedData.slice(0, 4);
+      };
+      reader.readAsBinaryString(file);
+    } else {
+        this.fileColumns = [];
+        this.fileDataRows = [];
+        this.filePlaceHolder = 'No File Selected';
+    }
+  }
+
+  generateDataString(rowObject) {
+    return Object.values(rowObject);
   }
 
   closeDialog(): void {
